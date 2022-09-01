70d3e6a9e9d0a5f05b1cd1a55aaed727859d105e
Ranula Ranatunga <ranularulz@gmail.com>
2022-09-01 14:25:27 +0530

    Initial Commit

---
+++ Forum/ForumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForumApp.swift
+//  Forum
+//
+//  Created by Ranula Ranatunga on 2022-09-01.
+//
+
+import SwiftUI
+
+@main
+struct ForumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
